230045a0a975c74d6b741f56822db813d48692a9
01jake <ang314256@gmail.com>
2025-05-26 12:05:55 -0700

    actualizacion de la campanita, ahora con un pager

--- GymDev/GymDev.Client/obj/Debug/net9.0/GymDev.Client.AssemblyInfo.cs
+++ GymDev/GymDev.Client/obj/Debug/net9.0/GymDev.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GymDev.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f885ad4308807bf8f7bb68883587967c1eb25cdc")]
 [assembly: System.Reflection.AssemblyProductAttribute("GymDev.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GymDev.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
